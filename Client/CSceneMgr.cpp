349352dd11b1ddd25b79f7a58a8d334601c85802
기태 이 <2017011293@gnu.ac.kr>
2023-01-21 12:33:56 +0900

    123

--- Client/CSceneMgr.cpp
+++ Client/CSceneMgr.cpp
@@ -45,7 +45,7 @@ void CSceneMgr::init()
 	m_arrScene[(UINT)SCENE_TYPE::GAMEOVER]->SetName(L"GameOver Scene");
 
 	// ÇöÀç ¾À ÁöÁ¤
-	m_pCurScene = m_arrScene[(UINT)SCENE_TYPE::PLAYGMAE];
+	m_pCurScene = m_arrScene[(UINT)SCENE_TYPE::START];
 	m_pCurScene->Enter();
 }
 
--- Client/CSceneOption.cpp
+++ Client/CSceneOption.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include "CSceneOption.h"
 
-
+#include "CBtnUI.h"
+#include "CScene_GameOver.h"
+#include "CResMgr.h"
 
 CSceneOption::CSceneOption()
 {
@@ -13,8 +15,16 @@ CSceneOption::~CSceneOption()
 
 void CSceneOption::Enter()
 {
+	CBtnUI* ExitBtn = new CBtnUI;
+	CTexture* exittex = CResMgr::GetInst()->LoadTexture(L"exitTex", L"texture\\Exit.bmp");
+	ExitBtn->SetPos(Vec2(20.f, 20.f));
+	ExitBtn->SetTexture(exittex);
+	ExitBtn->SetScale(Vec2(150.f, 100.f));
+	((CBtnUI*)ExitBtn)->SetClikedCallBack(this, (SCENE_MEMFUNC)&CScene_GameOver::ExitGame);
+	AddObject(ExitBtn, GROUP_TYPE::UI);
 }
 
 void CSceneOption::Exit()
 {
+	DeleteAll();
 }
